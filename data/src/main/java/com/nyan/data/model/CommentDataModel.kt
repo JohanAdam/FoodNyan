c485c5d7870f2d951a73e8e01ef077119036f83e
Johan <johanadam@mobius-digital.com>
2021-05-04 20:37:40 +0800

    - Add gson dependecies in data module. - Add serializablenamed in data model to show real world application in data model.

--- data/src/main/java/com/nyan/data/model/CommentDataModel.kt
+++ data/src/main/java/com/nyan/data/model/CommentDataModel.kt
@@ -1,7 +1,12 @@
 package com.nyan.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class CommentDataModel(
-    var name: String = "",
-    var comment: String = "",
-    var profile_picture_url: String = "",
+    @SerializedName("name")
+    var userName: String = "",
+    @SerializedName("comment")
+    var userComment: String = "",
+    @SerializedName("profile_picture_url")
+    var profilePictureUrl: String = "",
 )
--- data/src/main/java/com/nyan/data/model/RestaurantDataModel.kt
+++ data/src/main/java/com/nyan/data/model/RestaurantDataModel.kt
@@ -1,13 +1,24 @@
 package com.nyan.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class RestaurantDataModel(
+    @SerializedName("id")
     var id: Int,
-    var title: String = "",
-    var pictures: ArrayList<String>? = null,
-    var rating: Double = 0.0,
-    var address: String = "",
-    var lat: Double = 1.0,
-    var lng: Double = 1.0,
-    var desciption: String = "",
-    var comments: ArrayList<CommentDataModel>? = null
+    @SerializedName("title")
+    var restaurantTitle: String = "",
+    @SerializedName("pictures")
+    var restaurantPictures: ArrayList<String>? = null,
+    @SerializedName("rating")
+    var restaurantRating: Double = 0.0,
+    @SerializedName("address")
+    var restaurantAddress: String = "",
+    @SerializedName("lat")
+    var restaurantLat: Double = 3.1390,
+    @SerializedName("lng")
+    var restaurantLng: Double = 101.6869,
+    @SerializedName("desciption")
+    var restaurantDesciption: String = "",
+    @SerializedName("comments")
+    var restaurantComments: ArrayList<CommentDataModel>? = null
 )
